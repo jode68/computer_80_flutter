13b4ace091507ebab4da6751a87ca10e4b6fa312
jode68 <xumepao@yahoo.it>
2022-12-26 16:46:45 +0100

    v3

--- lib/lista_dati.dart
+++ lib/lista_dati.dart
@@ -14,18 +14,22 @@ final listaComputer80 = [
       './pic/Vic20_foto/Vic20_', 20),
   ListaModello(
       'Commodore 64', '1982', './pic/C64.jpg', 'https://it.wikipedia.org/wiki/Commodore_64', './pic/C64_foto/C64_', 20),
-  ListaModello('Commodore 16', '1984', './pic/C16.jpg', 'https://it.wikipedia.org/wiki/Commodore_16', '', 0),
-  ListaModello('Commodore 128', '1985', './pic/C128.jpg', 'https://it.wikipedia.org/wiki/Commodore_128', '', 0),
   ListaModello(
-      'Commodore Amiga 1000', '1985', './pic/Amiga1000.jpg', 'https://it.wikipedia.org/wiki/Amiga_1000', '', 0),
-  ListaModello('Commodore Amiga 500', '1987', './pic/Amiga500.jpg', 'https://it.wikipedia.org/wiki/Amiga_500', '', 0),
-  ListaModello(
-      'Commodore Amiga 2000', '1987', './pic/Amiga2000.jpg', 'https://it.wikipedia.org/wiki/Amiga_2000', '', 0),
-  ListaModello(
-      'Commodore Amiga 3000', '1990', './pic/Amiga3000.jpg', 'https://it.wikipedia.org/wiki/Amiga_3000', '', 0),
-  ListaModello(
-      'Commodore Amiga 1200', '1992', './pic/Amiga1200.jpg', 'https://it.wikipedia.org/wiki/Amiga_1200', '', 0),
-  ListaModello('Commodore Amiga 600', '1992', './pic/Amiga600.jpg', 'https://it.wikipedia.org/wiki/Amiga_600', '', 0),
+      'Commodore 16', '1984', './pic/C16.jpg', 'https://it.wikipedia.org/wiki/Commodore_16', './pic/C16_foto/C16_', 10),
+  ListaModello('Commodore 128', '1985', './pic/C128.jpg', 'https://it.wikipedia.org/wiki/Commodore_128',
+      './pic/C128_foto/C128_', 20),
+  ListaModello('Commodore Amiga 1000', '1985', './pic/Amiga1000.jpg', 'https://it.wikipedia.org/wiki/Amiga_1000',
+      './pic/Amiga1000_foto/Amiga1000_', 10),
+  ListaModello('Commodore Amiga 500', '1987', './pic/Amiga500.jpg', 'https://it.wikipedia.org/wiki/Amiga_500',
+      './pic/Amiga500_foto/Amiga500_', 10),
+  ListaModello('Commodore Amiga 2000', '1987', './pic/Amiga2000.jpg', 'https://it.wikipedia.org/wiki/Amiga_2000',
+      './pic/Amiga2000_foto/Amiga2000_', 10),
+  ListaModello('Commodore Amiga 3000', '1990', './pic/Amiga3000.jpg', 'https://it.wikipedia.org/wiki/Amiga_3000',
+      './pic/Amiga3000_foto/Amiga3000_', 10),
+  ListaModello('Commodore Amiga 1200', '1992', './pic/Amiga1200.jpg', 'https://it.wikipedia.org/wiki/Amiga_1200',
+      './pic/Amiga1200_foto/Amiga1200_', 10),
+  ListaModello('Commodore Amiga 600', '1992', './pic/Amiga600.jpg', 'https://it.wikipedia.org/wiki/Amiga_600',
+      './pic/Amiga600_foto/Amiga600_', 10),
   ListaModello('Atari ST512', '1985', './pic/ST512.jpg', 'https://it.wikipedia.org/wiki/Atari_ST', '', 0),
   ListaModello('Atari ST1024', '1986', './pic/ST1024.jpg', 'https://it.wikipedia.org/wiki/Atari_ST', '', 0),
   ListaModello('Sony Msx', '1983', './pic/MSX.jpg', 'https://it.wikipedia.org/wiki/MSX', '', 0),
